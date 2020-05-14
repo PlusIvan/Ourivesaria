18b78bd65e8e509b4e8a31a30028cfbac971c7d9
PlusIvan <ivan@ivanmoroz.net>
2020-05-14 19:57:25 +0100

    stuff
    photos, ejs, tailwind

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const server = app.listen(80, function(){
+    console.log(`Listen 80`);
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.json({ type: function() { return true; } }));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get(['/'], function (req, res) {
+    res.render('index');
+});
